592f1c84a152560a8a6b02b4358785654ecb6228
Alvaro Pata <helmanrm@gmail.com>
2020-11-23 19:18:39 +0100

    added utils package and updated .gitignore to ignore csv-files

---
+++ workforce_sentiment_monitoring/utils.py
@@ -0,0 +1,24 @@
+def remove_punctuation(text):
+    """remove puntuation of given text"""
+    # TODO
+    pass
+
+def remove_stopwords(text):
+    """remove stopwords"""
+    # TODO
+    pass
+
+def lemmatize(text):
+    """Lemmatize text"""
+    # TODO
+    pass
+
+def extract_negative(df):
+    """return df with negative reviews and their labels"""
+    # TODO
+    pass
+
+def extract_positive(df):
+    """return df with positive reviews and their labels"""
+    # TODO
+    pass
