8b0c02db3af1e593810cf7fd9cdc4f965a4f6d2b
Theresa Sporn <theresa.sporn@gmail.com>
2020-11-24 11:49:10 +0100

    took the basic preprocessing functions out of utils.py and created preprocessing.py and filled in the functions

---
+++ WorkforceSentimentMonitoring/preprocessing.py
@@ -0,0 +1,41 @@
+import string
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+    def lowercase(text):
+         """lowercase"""
+        lowercased = text.lower()
+        return lowercased
+
+    def remove_punctuation(text):
+         """remove punctuation"""
+        for punctuation in string.punctuation:
+            text = text.replace(punctuation, ' ')
+        return text
+
+    def remove_numbers(text):
+        """remove numbers"""
+        words_only = ''.join([i for i in text if not i.isdigit()])
+        return words_only
+
+    def remove_stopwords(text):
+        """remove stopwords + tokenize"""
+        stop_words = set(stopwords.words('english'))
+        tokenized = word_tokenize(text)
+        without_stopwords = [word for word in tokenized if not word in stop_words]
+        return without_stopwords
+
+    def lemmatize(text):
+        """Lemmatize text"""
+        lemmatizer = WordNetLemmatizer() # Initiate lemmatizer
+        lemmatized = [lemmatizer.lemmatize(word) for word in text] # Lemmatize
+        lemmatized_string = " ".join(lemmatized)
+        return lemmatized_string
+
+    def preprocessing(text):
+        preprocessed_text = lowercase(text)
+        preprocessed_text = remove_punctuation(text)
+        preprocessed_text = remove_stopwords(preprocessed_text)
+        preprocessed_text = lemmatize(preprocessed_text)
+        return preprocessed_text
--- WorkforceSentimentMonitoring/utils.py
+++ WorkforceSentimentMonitoring/utils.py
@@ -1,17 +1,35 @@
+def lowercase (text):
+    lowercased = text.lower()
+    return lowercased
+
 def remove_punctuation(text):
-    """remove puntuation of given text"""
-    # TODO
-    pass
+    import string
+    for punctuation in string.punctuation:
+        text = text.replace(punctuation, ' ')
+    return text
+
+def remove_numbers (text):
+    """remove numbers"""
+    words_only = ''.join([i for i in text if not i.isdigit()])
+    return words_only
 
 def remove_stopwords(text):
-    """remove stopwords"""
-    # TODO
-    pass
+    """remove stopwords + tokenize"""
+    from nltk.corpus import stopwords
+    from nltk import word_tokenize
+    stop_words = set(stopwords.words('english'))
+
+    tokenized = word_tokenize(text)
+    without_stopwords = [word for word in tokenized if not word in stop_words]
+    return without_stopwords
 
 def lemmatize(text):
     """Lemmatize text"""
-    # TODO
-    pass
+    from nltk.stem import WordNetLemmatizer
+    lemmatizer = WordNetLemmatizer() # Initiate lemmatizer
+    lemmatized = [lemmatizer.lemmatize(word) for word in text] # Lemmatize
+    lemmatized_string = " ".join(lemmatized)
+    return lemmatized_string
 
 def extract_negative(df):
     """return df with negative reviews and their labels"""
