ea493168ad8c8ee8107f8c27263eb73c1d48dc7b
Yunxi Zhang <zhang.yunxi.cn@gmail.com>
2020-11-24 12:41:36 +0100

    Delete other functions and keep the py file name

--- WorkforceSentimentMonitoring/utils.py
+++ WorkforceSentimentMonitoring/utils.py
@@ -1,19 +1,5 @@
 import pandas as pd
 
-def remove_punctuation(text):
-    """remove puntuation of given text"""
-    # TODO
-    pass
-
-def remove_stopwords(text):
-    """remove stopwords"""
-    # TODO
-    pass
-
-def lemmatize(text):
-    """Lemmatize text"""
-    # TODO
-    pass
 
 def extract_negative(df):
     """return df with negative reviews and their labels"""
