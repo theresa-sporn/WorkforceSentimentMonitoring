2e73508e4734b41d2a6799cf8a92f3963eb1244b
Yunxi Zhang <zhang.yunxi.cn@gmail.com>
2020-11-24 11:11:58 +0100

    Added first version of extraction neg and pos functions, please review it

--- WorkforceSentimentMonitoring/utils.py
+++ WorkforceSentimentMonitoring/utils.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def remove_punctuation(text):
     """remove puntuation of given text"""
     # TODO
@@ -15,10 +17,12 @@ def lemmatize(text):
 
 def extract_negative(df):
     """return df with negative reviews and their labels"""
-    # TODO
-    pass
+    df = df[['negatives']]
+    df.loc[:,'sentiment'] = 0 # 0=> negative
+    return df
 
 def extract_positive(df):
     """return df with positive reviews and their labels"""
-    # TODO
-    pass
+    df = df[['positives']]
+    df.loc[:,'sentiment'] = 1 # 1=> positive
+    return df
