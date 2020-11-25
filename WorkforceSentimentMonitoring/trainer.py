c6c5273847df6937aefbe9eaa83409690b0769a1
Yunxi Zhang <zhang.yunxi.cn@gmail.com>
2020-11-25 23:48:17 +0100

    First attemptto transform a trainer package, feature engineering functions are left out atm

--- WorkforceSentimentMonitoring/trainer.py
+++ WorkforceSentimentMonitoring/trainer.py
@@ -1,4 +1,4 @@
-import warning
+import warnings
 
 import pandas as pd
 import numpy as np
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from textblob import TextBlob
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LinearRegression, LogisticRegression
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.naive_bayes import MultinomialNB
@@ -15,65 +16,145 @@ from WorkforceSentimentMonitoring.preprocessing import preprocessing
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
+FEATURE_COLS = ['summary', 'positives', 'negatives', 'advice_to_mgmt']
+SCORE_COLS = ['score_1', 'score_2', 'score_3', 'score_4', 'score_5', 'overall']
+def feature_engineer(df):
+    # simple transformation from notebook, should be an extra feature package
+    # iterate over features and append results to df as new cols
+    result_scores = {}
+    for feature in FEATURE_COLS:
+        scores_dic = {}
+
+        for score in SCORE_COLS:
+            vectorizer = TfidfVectorizer()
+            X = vectorizer.fit_transform(df[feature].astype('U'))
+            y = df[score]
+            model = MultinomialNB()
+            model.fit(X, y)
+            df[f'{feature}_{score}'] = model.predict(X)
+            scores_dic[f'{score}'] = model.score(X, y)
+
+        result_scores[f'{feature}'] = scores_dic
+
+    # iterate over features and append results to df as new cols
+    scores_dic = {}
+    for score in SCORE_COLS:
+
+        result_scores = {}
+        for feature in FEATURE_COLS:
+            vectorizer = TfidfVectorizer()
+            X = vectorizer.fit_transform(df[feature].astype('U'))
+            y = df[score]
+            model = MultinomialNB()
+            model.fit(X, y)
+            df[f'{feature}_{score}'] = model.predict(X)
+            result_scores[f'{feature}'] = model.score(X, y)
+
+        scores_dic[f'{score}'] = result_scores
+        scores_df = pd.DataFrame(scores_dic).T
+    return df
+
+# Create a function to get the subjectivity
+def getSubjectivity(text):
+  return TextBlob(text).sentiment.subjectivity
+
+# Create a function to get the polarity
+def getPolarity(text):
+  return TextBlob(text).sentiment.polarity
+
+def feature_sentiment(df):
+
+    for feature in FEATURE_COLS:
+        df[f'subjectivity_{feature}'] = df[feature].astype('U').apply(getSubjectivity)
+        df[f'polarity_{feature}'] = df[feature].astype('U').apply(getPolarity)
+    return df
+
+# Create a function to get the total length of the reviews
+def get_lengths(df):
+    '''returns a df with columns with the length of the reviews'''
+    func = lambda x: len(x) if type(x) == str else 0
+    df['summary_length'] = df['summary'].apply(func)
+    df['postives_length'] = df['positives'].apply(func)
+    df['negatives_length'] = df['negatives'].apply(func)
+    df['advice_length'] = df['advice_to_mgmt'].apply(func)
+    df['combined_length'] = df['text_combined'].apply(func)
+    return df
+
+def scaler(df):
+    # scale new features
+    length_cols = [col for col in df.columns if 'length' in col]
+
+    for col in length_cols:
+        scaler = MinMaxScaler()
+        df[col] = scaler.fit_transform(df[[col]])
+
+    # select X
+    X = df.iloc[:, 11:]
+    # scale score features
+    pred_scores_cols = [col for col in X.columns if 'score' in col and not 'reg' in col]
+
+    for col in pred_scores_cols:
+        scaler = MinMaxScaler()
+        X[col] = scaler.fit_transform(X[[col]])
+    return X
+
+def linear_feature(X, df):
+    # linear regression with just the predictions for each model
+    for col in SCORE_COLS:
+        model = LinearRegression()
+        model.fit(X, df[col])
+        X[f'reg_{col}'] = model.predict(X)
+        scaler = MinMaxScaler()
+        X[f'reg_{col}'] = scaler.fit_transform(X[[f'reg_{col}']])
+    return X
+
 
 class Trainer(object):
-    ESTIMATOR = "MultinomialNB"
+    ESTIMATOR = "Logic"
+    # SCORE_COLS = ['score_1', 'score_2', 'score_3', 'score_4', 'score_5', 'overall']
+    # SCORE_COLS = ['work-balance', 'culture-values', 'career-opportunities', 'comp-benefits', 'senior-mgmt', 'overall']
 
     def __init__(self, X, y, **kwargs):
-        #self.pipeline = None
         self.kwargs = kwargs
-        self.split = self.kwargs.get("split", True)  # cf doc above
-        self.X_train = X
-        self.y_train = y
+        self.X = X
+        self.y = y
+        # self.df = self.get_lengths()
         # self.nrows = self.X_train.shape[0]  # nb of rows to train on
 
     def get_estimator(self):
         estimator = self.kwargs.get("estimator", self.ESTIMATOR)
-        if estimator == "MultinomialNB":
-            model = MultinomialNB()
-        elif estimator == "Linear":
-            model = LinearRegression()
+        if estimator == "Logic":
+            model = LogisticRegression(max_iter=1000)
         # elif estimator == "RandomForest":
         #     model = RandomForestRegressor()
         #     self.model_params = {   'n_estimators': [int(x) for x in np.linspace(start = 50, stop = 200, num = 10)],
         #         'max_features': ['auto', 'sqrt']}
         #      'max_depth' : [int(x) for x in np.linspace(10, 110, num = 11)]}
-        else:
-            model = LogisticRegression()
-        estimator_params = self.kwargs.get("estimator_params", {})
-        model.set_params(**estimator_params)
-        print(model.__class__.__name__)
+        # else:
+        #     model = Lasso()
+        # estimator_params = self.kwargs.get("estimator_params", {})
+        # model.set_params(**estimator_params)
+        # print(model.__class__.__name__)
         return model
 
+
     def set_pipeline(self):
+        """implement the feature pipelines"""
+        pass
 
-        # feature_engineer = make_pipeline(get_subjectivity(column='text'),
-        #                               TextBlob())
-
-        # pipe_polarity = make_pipeline(get_polarity(column='text'), TextBlob())
-
-        # features_encoder = ColumnTransformer([
-        #     ('text', DistanceTransformer(**DIST_ARGS), list(DIST_ARGS.values())),
-        #     ('', time_features, ['']),
-        #     )
-        # ])
-
-        self.pipeline = Pipeline(steps=[
-            ('tfidf', TfidfVectorizer()),
-            ('rgs', self.get_estimator())])
-
-
-    def train(self):
-        tic = time.time()
-        self.set_pipeline()
-        self.pipeline.fit(self.X_train, self.y_train)
-
-    def evaluate(self, X_test, y_test):
-        if self.pipeline is None:
-            raise ("Cannot evaluate an empty pipeline")
-        y_pred = self.pipeline.predict(X_test)
-        #rmse = compute_rmse(y_pred, y_test)
-        return
+    def train(self, df):
+        SCORE_COLS = ['score_1', 'score_2', 'score_3', 'score_4', 'score_5', 'overall']
+        """iterate features, train and predict targets"""
+        predictions = pd.DataFrame()
+        pred_scores = {}
+        for target in SCORE_COLS:
+            # self.set_pipeline(): after set the pipelines
+            self.model = self.get_estimator()
+            y = df[target] # need feature packages
+            self.model.fit(self.X, y)
+            predictions[target] = self.model.predict(self.X)
+            pred_scores[target] = self.model.score(self.X, y)
+        return (predictions, pred_scores)
 
 
 if __name__ == "__main__":
@@ -87,5 +168,5 @@ if __name__ == "__main__":
     y_val = holdout(data, target)[3]
 
     # Train and save model, locally and
-    t = Trainer(X=X_train, y=y_train, estimator="RandomForest")
-    t.train()
+    #t = Trainer(X=X_train, y=y_train, estimator="Linear")
+    #t.train()
--- tests/lib_test.py
+++ tests/lib_test.py
@@ -5,7 +5,7 @@ import os
 import WorkforceSentimentMonitoring
 import pandas as pd
 # Import from our lib
-from WorkforceSentimentMonitoring.lib import get_data
+from WorkforceSentimentMonitoring.lib import get_df
 import pytest
 
 
@@ -17,3 +17,12 @@ import pytest
 #     assert df.shape == (999, 142)
 #     out = clean_data(df)
 #     assert out.shape == (985, 119)
+
+def test_get_df():
+    datapath = os.path.dirname(os.path.abspath(WorkforceSentimentMonitoring.__file__)) + '/raw_data'
+    df = pd.read_csv('{}/data.csv.gz'.format(datapath))
+    first_cols = ['summary', 'positives', 'negatives', 'advice_to_mgmt', 'work-balance', 'culture-values']
+    assert list(df.columns)[:6] == first_cols
+    assert df.shape == (52815, 11)
+    out = get_df()
+    assert out.shape == (52815, 11)
