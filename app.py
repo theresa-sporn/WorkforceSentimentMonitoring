59c7cb4a560f11e8b24733e173b75ffabdeb48c8
Celine Gianduzzo <cgianduzzo@gmail.com>
2020-11-26 17:29:51 +0100

    changes regarding fronend - sidebar and piecharts

--- app.py
+++ app.py
@@ -9,22 +9,6 @@ import matplotlib.pyplot as plt
 
 from WorkforceSentimentMonitoring.data import get_data, merge
 
-st.title('Workforce Sentiment Dashbord')
-
-st.sidebar.title("Visualization Selector")
-
-# get the dataframe
-submission, train, test = get_data()
-df = merge(submission, train, test)
-
-categories = ['work-balance', 'culture-values', 'career-opportunities','comp-benefits', 'senior-mgmt', 'overall']
-df = df[categories]
-
-# display in streamlit
-st.dataframe(df[:1])
-
-topics = ['work-balance', 'culture-values', 'career-opportunities','comp-benefits', 'senior-mgmt']
-
 # get the dataframe
 submission, train, test = get_data()
 df = merge(submission, train, test)
@@ -32,6 +16,9 @@ df = merge(submission, train, test)
 categories = ['work-balance', 'culture-values', 'career-opportunities','comp-benefits', 'senior-mgmt', 'overall']
 topics = ['work-balance', 'culture-values', 'career-opportunities','comp-benefits', 'senior-mgmt']
 df = df[categories]
+
+
+# count pos, neg, neutral reviews
 
 pos_counts = df[df>=4].count()
 neg_counts = df[df<=2].count()
@@ -47,33 +34,46 @@ for category in categories:
     df_counts[f'{category}_counts'] = (df_counts[f'{category}']/total_counts)
 
 
+# creating pie charts according to categories
+figures = []
+for category in categories:
+
+    labels = ['positive', 'negative', 'neutral']
+    values = list(df_counts[f'{category}'])
+    index_max = np.argmax(values)
+    pull = [0,0,0]
+    pull[index_max] = 0.2
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, pull=pull, textinfo='label+percent', title=category)])
+    #fig.show()
+    #st.plotly_chart(fig)
+    figures.append(fig)
 
 
-st.sidebar.checkbox("Show Analysis by Category", True, key=1)
-select = st.sidebar.selectbox('Select a Category', topics)
+# streamlit
+
+st.title('Workforce Sentiment Dashbord')
+st.markdown('### Welcome to your *monitoring dashboard*')
+st.sidebar.title("Visualization Selector")
+
+    #st.dataframe(df[:1])
+if not st.checkbox('Hide Graph', False, key=1):
+    st.plotly_chart(figures[categories.index('overall')])
 
 
-if st.sidebar.checkbox("Show Analysis by Category", True, key=2):
+if st.sidebar.checkbox("Show Analysis of selected Category", False, key=1):
+    select = st.sidebar.selectbox('Select a Category', topics)
+
+
+if st.sidebar.checkbox("Show Analysis of selected Category", False, key=2):
     st.markdown("## **Analysis**")
-    #st.markdown("###" (select))
-    if not st.checkbox('Hide Graph', False, key=1):
+    st.markdown("### %s " % (select))
+    if not st.checkbox('Hide Graph', False, key=2):
 
-        figures = []
+        'Review topic:', select
 
-        for category in categories:
-
-            labels = ['positive', 'negative', 'neutral']
-            values = list(df_counts[f'{category}'])
-            # print(values)
-            index_max = np.argmax(values)
-            # print(index_max)
-            pull = [0,0,0]
-            pull[index_max] = 0.2
-            # print(pull)
-            fig = go.Figure(data=[go.Pie(labels=labels, values=values, pull=pull)])
-            #fig.show()
-            st.plotly_chart(fig)
-            figures.append(fig)
+        for topic in topics:
+            if topic == select:
+                st.plotly_chart(figures[topics.index(topic)])
 
 #figures
 
