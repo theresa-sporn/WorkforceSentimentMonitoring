03435aaf68f6a929eaecdc053e5e863f2ffabcd7
Yunxi Zhang <zhang.yunxi.cn@gmail.com>
2020-11-30 16:06:36 +0100

    Modified test data py

--- test_data/backup_test_data.py
+++ test_data/backup_test_data.py
@@ -5,6 +5,15 @@ def get_test_data():
     path = 'http://s3.amazonaws.com/assets.datacamp.com/production/course_935/datasets/500_amzn.csv'
     df = pd.read_csv(path)
     df = df.drop(columns=['pg_num', 'url'])
+    df = df.rename(
+        columns={
+            "pros": "positives",
+            "cons": "negatives",
+        }
+    )
+
+    text_columns = ["positives", "negatives"]
+    df["review"] = df[text_columns].astype("U").agg(" ".join, axis=1)
     return df
 
 if __name__ == "__main__":
