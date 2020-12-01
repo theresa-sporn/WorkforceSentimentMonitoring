f7117889da8c94eb5d1686549a079389e51e677c
Theresa Sporn <theresa.sporn@gmail.com>
2020-12-01 14:04:40 +0100

    created lda.py file containing the coder for pyLDAvis

--- WorkforceSentimentMonitoring/lda.py
+++ WorkforceSentimentMonitoring/lda.py
@@ -1,40 +1,32 @@
-import gensim
 import gensim.corpora as corpora
-from gensim.utils import simple_preprocess
+from gensim.utils import lemmatize, simple_preprocess
 from gensim.models import CoherenceModel
 import matplotlib.pyplot as plt
-import pprint as pp
+from gensim.corpora import Dictionary
 import pyLDAvis
 import pyLDAvis.gensim
-from preprocessing import preprocessing
-from utils import extract_negatives, extract_positives
 
-# def get_negatives(text):
-#     negatives = df['overall_score'] <3
-#     return negatives
+def extract_negative(df):
+    """return df with negative reviews and their labels"""
+    negatives = df[['negatives']]
+    return negatives
 
-# def get_positives(text):
-#     positives = df['overall_score'] >3
-#     return positives
+def extract_positive(df):
+    """return df with negative reviews and their labels"""
+    positive = df[['positives']]
+    return positives
 
-# def get_neutrals(text):
-#     neutrals = df['overall_score'] == 3
-#     return neutrals
 
-text = preprocessing(text)
-positives = extract_positives(text)
-negatives = extract_negatives(text)
-#texts = [positives, negatives]
+id2word = corpora.Dictionary(negatives_tokenized)
+dictionary = id2word
+texts = negatives_tokenized
+corpus = [id2word.doc2bow(text) for text in texts]
+ldamallet = gensim.models.ldamodel.LdaModel(corpus=corpus, num_topics=2, id2word=id2word, iterations=100)
+coherence_model_ldamallet = CoherenceModel(model=ldamallet, texts=texts, dictionary=id2word, coherence='c_v')
+coherence_ldamallet = coherence_model_ldamallet.get_coherence()
 
-def make_pyLDAvis(dictionary, corpus, texts, limit, start=2, step=1):
-    id2word = corpora.Dictionary(tokenized)
-    positives or negatives #change to taking preprocessed text from positive/negative df
-    corpus = [id2word.doc2bow(text) for text in texts]
-    ldamallet = gensim.models.ldamodel.LdaModel(corpus=corpus, num_topics=2, id2word=id2word, iterations=100)
-    coherence_model_ldamallet = CoherenceModel(model=ldamallet, texts=texts, dictionary=id2word, coherence='c_v')
-    coherence_ldamallet = coherence_model_ldamallet.get_coherence()
-    #model_list, coherence_values = compute_coherence_values(dictionary=id2word, corpus=corpus, texts=texts, start=4, limit=16, step=2)
 
+def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=1):
     coherence_values = []
     model_list = []
     for num_topics in range(start, limit, step):
@@ -44,11 +36,15 @@ def make_pyLDAvis(dictionary, corpus, texts, limit, start=2, step=1):
         coherence_values.append(coherencemodel.get_coherence())
     return model_list, coherence_values
 
-    max_y = max(coherence_values)
-    max_x = coherence_values.index(max(coherence_values))
-    optimal_model = model_list[coherence_values.index(max(coherence_values))]
+model_list, coherence_values = compute_coherence_values(dictionary=id2word, corpus=corpus, texts=texts, start=4, limit=16, step=2)
 
-    #optimal_num_topics = coherence_values.index(max(coherence_values))
-    pyLDAvis.enable_notebook()
-    vis = pyLDAvis.gensim.prepare(optimal_model, corpus, id2word)
-    return vis
+max_y = max(coherence_values)  # Find the maximum y value
+max_x = coherence_values.index(max(coherence_values))  # Find the x value corresponding to the maximum y value
+#xmax = x[numpy.argmax(y)]
+
+optimal_model = model_list[coherence_values.index(max(coherence_values))]
+
+pyLDAvis.enable_notebook()
+vis = pyLDAvis.gensim.prepare(optimal_model, corpus, id2word)
+vis
+
