fdb042c18576de4d1688e3ae64d31748a2eca202
Alvaro Pata <helmanrm@gmail.com>
2020-12-22 10:14:03 +0100

    export embeddings df to pickle files

--- WorkforceSentimentMonitoring/embedding.py
+++ WorkforceSentimentMonitoring/embedding.py
@@ -6,6 +6,8 @@ import numpy as np
 import swifter
 import pickle
 import os
+import numpy as np
+import pandas as pd
 
 embedder = DocumentPoolEmbeddings([FlairEmbeddings('news-forward-fast'),
                                    FlairEmbeddings('news-backward-fast')])
@@ -16,27 +18,35 @@ def embed(text, embedder):
 
 
 if __name__ == '__main__':
-    submission, train, test = get_data()
-    df = merge(submission, train, test)
-    df = drop_wrong_language(df, "review")
+    # submission, train, test = get_data()
+    # df = merge(submission, train, test)
+    # df = drop_wrong_language(df, "review")
 
+    # path = os.path.split(os.path.abspath('__file__'))[0]
+    # file = os.path.join(path, 'pickle_files/reviews_eng.p')
+    # with open(file, 'wb') as f:
+    #     pickle.dump(df, f)
     path = os.path.split(os.path.abspath('__file__'))[0]
     file = os.path.join(path, 'pickle_files/reviews_eng.p')
-    with open(file, 'wb') as f:
-        pickle.dump(df, f)
-
-    # X = df[['review']]
-    # y = df.iloc[:, -7:-1]
+    with open(file, 'rb') as f:
+        df = pickle.load(f)
+    print(df)
 
     # X_tmp, y_tmp = (X.sample(1, random_state=21).reset_index(drop=True),
     #                y.sample(1, random_state=21).reset_index(drop=True))
+    
+    chunks = np.array_split(df, 10)
+    for idx, chunk in enumerate(chunks, 1):
+        print(type(chunk))
+        chunk['embedding'] = chunk['review'].swifter.allow_dask_on_strings()\
+                             .apply(lambda x: embed(x, embedder))
+        file = os.path.join(path, f'pickle_files/embeddings_chunk{idx}')
+        with open(file, 'wb') as f:
+            pickle.dump(chunk, f)
 
-    # X_tmp['embedding'] = X_tmp['review'].swifter.allow_dask_on_strings()\
-    #                      .apply(lambda x: embed(x, embedder))
-
-    # tmp = X_tmp.join(y_tmp)
+    
 
     # path = os.path.split(os.path.abspath('__file__'))[0]
-    # file = os.path.join(path, '/pickle_files/tmp_10k.p')
+    # file = os.path.join(path, 'pickle_files/embeddings_all.p')
     # with open(file, 'wb') as f:
-    #     pickle.dump(tmp, f)
+    #     pickle.dump(df, f)
